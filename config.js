12a9dabb80aeb89741bd122a9a13706512438622
youssef-devx <youssefboulalq@gmail.com>
2022-12-31 20:01:10 +0100

    Fix api proplem

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production"
 
 export const API_URL = dev
   ? "http://localhost:3000/api/posts?searchKeyword="
-  : "https://api-project-next/api/posts"
+  : "https://api-project-next/api/posts?searchKeyword="
